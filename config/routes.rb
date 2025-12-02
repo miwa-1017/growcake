5ee02dea0151254f939cdb0dda9fd422e4579839
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-12-02 06:16:01 +0000

    フェーズ1最終調整＆ユーザビリティ改善対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
+
   get 'homes/top'
-  get 'homes/about'
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
+  authenticate :user do
+    get '/mypage', to: 'users#show', as: 'mypage'
+  end
+
+  resources :users, only: [:show]  
   resources :posts
   resources :exercise_logs, only: [:index, :new, :create]
 end
