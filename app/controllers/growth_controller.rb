4a5bd8765ca8fc32ea263feeda0c1365b66e1b3c
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-12-06 04:49:11 +0000

    ケーキ成長ページ機能改善・演出条件修正

--- app/controllers/growth_controller.rb
+++ app/controllers/growth_controller.rb
@@ -3,7 +3,10 @@ class GrowthController < ApplicationController
 
   def show
     @user = current_user
-    @stage = @user.current_stage
+    stages = User::GROWTH_STAGES[@user.cake_type.to_sym]
     @total_points = @user.total_growth_point
+
+    # ★成長段階の決め方（境界値以内で最初に一致した位置）
+    @stage = stages.index { |point| @total_points < point } || stages.length - 1
   end
 end
