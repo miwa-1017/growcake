405537c9d05b4c71e2a8e0702db0ec0a3ceda5fb
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-12-11 11:40:40 +0000

    Fix

--- db/migrate/20251209043830_add_cake_type_at_posts.rb
+++ db/migrate/20251209043830_add_cake_type_at_posts.rb
@@ -1,6 +1,6 @@
 class AddCakeTypeAtPosts < ActiveRecord::Migration[6.1]
   def change
-add_column :posts, :cake_type_at_post, :stiring
+    add_column :posts, :cake_type_at_post, :string
   
   end
 end
