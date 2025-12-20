5fb13057344e030b2f3615af73ba9b181786526e
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-12-20 05:19:42 +0000

    Fix foreign key error when deleting post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
-  has_many :growth_records
+  has_many :growth_records, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :notifications, as: :notifiable, dependent: :destroy
